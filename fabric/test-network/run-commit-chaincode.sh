e87a887d2dc78fef410d7ac5074bfd8ad2195f2d
Percival Lucena <plucena@gmail.com>
2025-01-26 16:04:49 -0300

    updates

---
+++ fabric/test-network/run-commit-chaincode.sh
@@ -0,0 +1,1 @@
+peer lifecycle chaincode checkcommitreadiness --channelID hometransfer --name HomeTransfer --version 1.0 --sequence 1 --output json --init-required
---
+++ fabric/test-network/run-package.sh
@@ -0,0 +1,1 @@
+peer lifecycle chaincode package hometransfer.tar.gz --path ../chaincode/hometransfer/build/install/hometransfer --lang java --label hometransfer_1
